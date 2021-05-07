3b5e923c8884ee284b3ca74550f7e6152fbb5bb7
fan <fan@ditmer.dk>
2021-05-07 13:48:12 +0200

    - Protecting and api using client credentials done

--- src/IdentityServer/Config.cs
+++ src/IdentityServer/Config.cs
@@ -9,18 +9,31 @@ namespace IdentityServer
 {
     public static class Config
     {
-        public static IEnumerable<IdentityResource> IdentityResources =>
-            new IdentityResource[]
-            { 
-                new IdentityResources.OpenId()
+        public static IEnumerable<ApiScope> ApiScopes =>
+            new List<ApiScope>
+            {
+                new ApiScope("api1", "Frands API") // If you will be using this in production it is important to give your API a logical name. Developers will be using this to connect to your api though your Identity server. It should describe your api in simple terms to both developers and users.
             };
 
-        public static IEnumerable<ApiScope> ApiScopes =>
-            new ApiScope[]
-            { };
-
         public static IEnumerable<Client> Clients =>
-            new Client[] 
-            { };
+        new List<Client>
+        {
+            new Client
+            {
+                ClientId = "client", // You can think of the ClientId and the ClientSecret as the login and password for your application itself
+
+                // no interactive user, use the clientid/secret for authentication
+                AllowedGrantTypes = GrantTypes.ClientCredentials,
+
+                // secret for authentication
+                ClientSecrets =
+                {
+                    new Secret("secret".Sha256()) // This secret should be stored a better place
+                },
+
+                // scopes that client has access to
+                AllowedScopes = { "api1" }
+            }
+        };
     }
 }
